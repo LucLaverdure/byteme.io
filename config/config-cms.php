ee48c5e4cf0b04bd35d2a751ff836ab170ba5f60
Luc Laverdure <contact@luclaverdure.com>
2018-02-27 18:07:05 -0500

    Delete config-cms.php

--- config/config-cms.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('CMS_DB_HOST', 'db725689271.db.1and1.com');
-	define('CMS_DB_USER', 'dbo725689271');
-	define('CMS_DB_PASS', 'kajsd*&?nbmf6514FDAS');
-	define('CMS_DB_NAME', 'db725689271');
-?>
